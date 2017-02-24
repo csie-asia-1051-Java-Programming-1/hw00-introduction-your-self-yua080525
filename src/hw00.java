31dc51b21d9679a8373368b2072a87cd0edea8d1
yua080525 <105021111@live.asia.edu.tw>
2017-02-24 14:56:03 +0800

    hw submit

---
+++ src/hw00.java
@@ -0,0 +1,16 @@
+/*
+* Topic: 自我介紹
+* Date: 2017/02/24
+* Author: 105021111 林孟璇
+*/
+public class hw00 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("姓名:林孟璇");
+		System.out.println("學號:105021111");
+		System.out.println("班級:資工系資電組1A");
+
+	}
+
+}
